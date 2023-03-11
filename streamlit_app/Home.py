2410e52bff2a5950e51de767edc6abd0ef74eeb8
SMoralesS <santiago.morales2@udea.edu.co>
2023-03-11 20:03:29 +0100

    Update Home page with description and signature.

--- streamlit_app/Home.py
+++ streamlit_app/Home.py
@@ -1,4 +1,4 @@
-from streamlit_app.utils.home_page import *
+from utils.home_page import *
 
 st.set_page_config(
     page_title="Home",
@@ -6,19 +6,10 @@ st.set_page_config(
 )
 
 def main():
-    import os
-    st.write(os.listdir())
     home_page = HomePage()
     home_page.add_logo()
     home_page.add_signature()
     home_page.add_intro()
-    text =  "<ul>\
-            <li>Coffee</li>\
-            <li>Tea</li>\
-            <li>Milk</li>\
-        </ul>" 
-
-    st.components.v1.components.html(text, width=200, height=200)
 
 if __name__ == "__main__":
     main()
