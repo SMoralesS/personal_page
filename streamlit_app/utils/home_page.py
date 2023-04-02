e8d4b48e6878e36f463ad1343688d769325f44d5
SMoralesS <santiago.morales2@udea.edu.co>
2023-04-02 18:18:57 +0200

    Update utils

--- streamlit_app/utils/home_page.py
+++ streamlit_app/utils/home_page.py
@@ -1,11 +1,16 @@
 import json
 import streamlit as st
+import utils.utils
 
 
 class HomePage:
-    def __init__(self, data_path: str = './streamlit_app/utils/personal_data.json') -> None:
-        with open(data_path, 'r') as data_file:
+    def __init__(
+        self, 
+        personal_info_data_path: str = './streamlit_app/utils/personal_data.json',
+        ) -> None:
+        with open(personal_info_data_path, 'r') as data_file:
             self.personal_data = json.load(data_file)
+        
 
     def add_logo(self):
         st.markdown(
@@ -37,3 +42,26 @@ class HomePage:
 
     def add_intro(self):
         st.write(self.personal_data.get('personal_description', ''))
+    
+    def add_socials(self):
+        linkedin_url = self.personal_data.get('linkedin', '')
+        linkedin_title = 'Linkedin' if linkedin_url else ''
+        linkedin_logo = 'https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg' if linkedin_url else ''
+        
+        github_url = self.personal_data.get('github', '')
+        github_title = 'Github' if linkedin_url else ''
+        github_logo = 'https://cdn-icons-png.flaticon.com/512/3291/3291695.png' if github_url else ''
+        
+        with st.sidebar:
+            cols = st.columns(4)
+            with cols[1]:
+                st.markdown(
+                    utils.utils.img_url_to_html(linkedin_logo, linkedin_url, linkedin_title, '80%', '80%'),
+                    unsafe_allow_html=True
+                )
+                 
+            with cols[2]:
+                st.markdown(
+                    utils.utils.img_url_to_html(github_logo, github_url, github_title, '70%', '70%'),
+                    unsafe_allow_html=True
+                )
--- streamlit_app/utils/utils.py
+++ streamlit_app/utils/utils.py
@@ -3,3 +3,20 @@ import streamlit as st
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+def img_url_to_html(img_url: str, href: str='', title: str='', height: str=None, width: str=None):
+    '''
+    Input: 
+    img_url: location of the image. 
+    href: Hyperlink you want to link to.
+    title: Title of the image.
+    height: height of the image as string including the dimensionality (px, %, ...).
+    width: width of the image as string including the dimensionality (px, %, ...).
+    '''
+    href = href if href else '#'
+    html = f"""
+        <a href=\"{href}\">
+        <img src=\"{img_url}\" alt=\"{title}\" width=\"{width}\" height=\"{height}\">
+        </a>
+    """
+    return html
